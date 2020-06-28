cd7cd7713e18a0d0c675fdf121c94a34753d1b02
KodJesus <adam.v.hoewijk@gmail.com>
2020-06-28 21:22:05 +0200

    Plot the great depression and current situation at the same time, also added column to identify recession

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 import plotly.graph_objs as go
+import plotly.express as px
 from pandas_datareader._utils import RemoteDataError
 from pandas_datareader import data
 
 START_DATE = '2020-01-01'
 END_DATE = datetime.now().strftime('%Y-%m-%d')
 Ticker = '^GSPC'
+
 
 # Return a data frame with only the values after a all time high
 def all_time_high(df):
@@ -27,44 +29,50 @@ def all_time_high(df):
     df['day'] = np.arange(len(df))
     return df
 
+
 # Select the adjusted closed, fills nill and makdes sure the dates are within range
 def clean_data(df):
     dateRange = pd.date_range(start=START_DATE, end=END_DATE)
     clean_data = df['Adj Close'].reindex(dateRange)
     return clean_data.fillna(method='ffill').to_frame()
 
+
 # Collects ticker data from yahoo and returns it cleaned
 def get_data(ticker):
     try:
         df = data.DataReader(ticker,
-                                    'yahoo',
-                                    START_DATE,
-                                    END_DATE)
+                             'yahoo',
+                             START_DATE,
+                             END_DATE)
         return clean_data(df)
 
     except RemoteDataError:
         print('no data Found for {t}'.format(t=ticker))
 
-df = get_data(Ticker)
-df = all_time_high(df)
-trace_close = go.Scatter(x=list(df['day']),
-                         y=list(df['Percentage']),
-                         name='Close',
-                         line=dict(color="#f44242"))
-data =[trace_close]
-layout = dict(title="Stock Chart",
-           showLegend=False)
-fig = dict(data=data, layout=layout)
+
+def current_df():
+    df = get_data(Ticker)
+    df = all_time_high(df)
+    df['Res'] = 'Current'
+    return df
+
+
+def merged_fig(df_name, current_df):
+    max_day = current_df[current_df['day'] == current_df['day'].max()].iloc[0]['day']
+    df = pd.read_csv(df_name, index_col='Date')
+    df = df.drop(df[df.day > max_day * 3].index)
+    return px.line(pd.concat([df, current_df]), x="day", y="Percentage", color='Res')
+
+
+gd_fig = merged_fig('The great depression.csv', current_df())
 
 app = dash.Dash()
 
 app.layout = html.Div([
-    html.Div(html.H1(children='hello world')),
-    html.Div(
-        dcc.Graph(id="Stock Chart",
-                  figure=fig)
-    )
+    html.H1(children='Current percentage change of S&P500 displayed alongside historical change'),
+    html.Button('The great depression', id='gd_button'),
+    dcc.Graph(figure=gd_fig,
+              id='graph')
 
-])
 
 app.run_server(debug=True)
